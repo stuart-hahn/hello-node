7e2c52b17aaa55e33c70fe9d592869a5bddd83f2
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-21 18:27:03 -0400

    feat: use try catch to handle server error

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const port = 3000;
 
 // Create a server that sends "Hello, World!" response
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+  try {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, World!\n");
+  } catch {
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+    console.error("An error occurred: ", error);
+  }
 });
 
 server.listen(port, hostname, () => {
